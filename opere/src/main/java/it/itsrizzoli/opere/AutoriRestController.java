962295b8944bfe06291a9a2391e77860d9bfbfdb
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2026-06-03 17:50:46 +0200

    commit

---
+++ opere/src/main/java/it/itsrizzoli/opere/AutoriRestController.java
@@ -0,0 +1,26 @@
+package it.itsrizzoli.opere;
+
+import it.itsrizzoli.opere.DAO.AutoriDAO;
+import it.itsrizzoli.opere.Model.AutoriSearch;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutoriRestController {
+    @Autowired
+    AutoriDAO autoriDAO;
+
+    @GetMapping("/autori/search")
+    @ResponseBody
+    public List<AutoriSearch> searchAutori(@RequestParam("q") String q) {
+        return autoriDAO.searchFullName(q).stream()
+            .map(a -> new AutoriSearch(a.getId(), a.getNome(), a.getCognome()))
+            .toList();
+    }
+}
